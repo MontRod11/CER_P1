2b677cf008fe7d5e5855e8620dc3bfd8e0a34d0d
MontRod11 <laura.montalvo@edu.uah.es>
2021-10-21 10:01:21 +0000

    v2

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
-apt update && apt upgrade -y
-apt install python3-pip
+sudo apt update && apt upgrade -y
+sudo apt install python3-pip
 pip3 install -r requirements.txt
 echo 'export PATH="/home/$USER/.local/bin:$PATH""' >> .bashrc
 source .bashrc
 curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
-apt update
-apt install -y elasticsearch
+sudo apt update
+sudo apt install -y elasticsearch
+pip install elasticsearch
