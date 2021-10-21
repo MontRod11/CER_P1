935c94ed3819d490e7433f0ba2de1d251589ae0c
MontRod11 <laura.montalvo@edu.uah.es>
2021-10-21 09:00:09 +0000

    prueba_flask_expreg

---
+++ flask_prueba.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import re, requests
+app = Flask(__name__)
+
+@app.route("/")
+def num_aleatorio():
+    return re.compile('\d*\.?\d*<br>').findall(requests.get('https://www.numeroalazar.com.ar/').text)[0][:-4]
+
+if __name__ == "__main__":
+    #app.run()
+    app.run(host='0.0.0.0', port=5000, debug=True)
