a85c51b6b6a7306c6eef82fec009388a3d470012
MontRod11 <laura.montalvo@edu.uah.es>
2021-10-21 10:53:29 +0000

    prueba_template_flaskv4

--- flask_prueba.py
+++ flask_prueba.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template 
 from elasticsearch import Elasticsearch
 import re, requests
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,9 +11,10 @@ def num_aleatorio():
 def hello():
     return render_template('/laura/index.html',num_aleat=re.compile('\d*\.?\d*<br>').findall(requests.get('https://www.numeroalazar.com.ar/').text)[0][:-4])
 
-@app.route("/laura")
-def hello_laura():
-    return render_template('/laura/index.html',num_aleat=re.compile('\d*\.?\d*<br>').findall(requests.get('https://www.numeroalazar.com.ar/').text)[0][:-4])
+#@app.route("/laura")
+#def hello_laura():
+#    return render_template('/laura/index.html',num_aleat=re.compile('\d*\.?\d*<br>').findall(requests.get('https://www.numeroalazar.com.ar/').text)[0][:-4])
+
 if __name__ == "__main__":
     
     #app.run()
