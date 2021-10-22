ac81098d6fe2f9cc1aec0a9f71c0c4024433c46b
MontRod11 <laura.montalvo@edu.uah.es>
2021-10-22 08:34:35 +0000

    prueba_elasticsearch_accessv2

--- elasticsearch_prueba.py
+++ elasticsearch_prueba.py
@@ -2,15 +2,54 @@ from flask import Flask, render_template
 from elasticsearch import Elasticsearch
 import re, requests
 
-elastic_client = Elasticsearch(hosts=["localhost"])
+elastic_client = Elasticsearch([{'host':'localhost','port':9200}])
+#elastic_client.delete(index="tabla1", id='_all')
 for i in range(5):
     r = re.compile('\d*\.?\d*<br>').findall(requests.get('https://www.numeroalazar.com.ar/').text)[0][:-4]
-    elastic_client.index(index='tabla1', id=i, document={'numero':float(r)})
+    elastic_client.index(index="tabla2", id=i, document={'numero':float(r)})
 
-
-data = elastic_client.get(index="tabla1",id=3)
+data = elastic_client.get(index="tabla2",id=3)
 print(str(data['_source']['numero']))
-diccionario_bbdd = elastic_client.search(index='tabla1')
-print(diccionario_bbdd['hits']['hits']['_source']['numero'])
-def media():
-    values = diccionario_bbdd['_source']['numero']
+elastic_client.indices.refresh(index="tabla2")
+forma = 1
+if forma ==0:
+    diccionario_bbdd = elastic_client.search(index="tabla2")
+    #print(diccionario_bbdd['hits']['hits']['_source']['numero'])
+    def media(sum_values,num_values):
+        return sum_values/num_values
+        
+    def get_values(dict):
+        acum = 0
+        for i in range(len(dict['hits']['hits'])):
+            value = diccionario_bbdd['hits']['hits'][i]['_source']['numero']
+            print(str(i)+' numero de la lista: '+str(value))
+            acum = acum + value
+        return acum, len(dict['hits']['hits'])
+
+    sum_values, num_values = get_values(diccionario_bbdd)
+    mean = media(sum_values,num_values)
+
+    print('La media es:'+str(mean))
+    print(sum_values)
+    print(num_values)
+else:
+    diccionario_bbdd = elastic_client.search(index="tabla2", filter_path=['hits.hits._source'])
+    def media(sum_values,num_values):
+        return sum_values/num_values
+        
+    def get_values(dict):
+        acum = 0
+        for i in range(len(dict['hits']['hits'])):
+            value = diccionario_bbdd['hits']['hits'][i]['_source']['numero']
+            print(str(i)+' numero de la lista: '+str(value))
+            acum = acum + value
+        return acum, len(dict['hits']['hits'])
+
+    sum_values, num_values = get_values(diccionario_bbdd)
+    mean = media(sum_values,num_values)
+
+    print('La media es:'+str(mean))
+    print(sum_values)
+    print(num_values)
+
+
