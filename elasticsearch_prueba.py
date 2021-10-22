d3955d494f94211d54a70ad14755a40d82e65112
MontRod11 <laura.montalvo@edu.uah.es>
2021-10-22 11:01:58 +0000

    prueba_elasticsearch_accessv3

--- elasticsearch_prueba.py
+++ elasticsearch_prueba.py
@@ -4,17 +4,17 @@ import re, requests
 
 elastic_client = Elasticsearch([{'host':'localhost','port':9200}])
 #elastic_client.delete(index="tabla1", id='_all')
+tabla = "tabla1"
 for i in range(5):
     r = re.compile('\d*\.?\d*<br>').findall(requests.get('https://www.numeroalazar.com.ar/').text)[0][:-4]
-    elastic_client.index(index="tabla2", id=i, document={'numero':float(r)})
-
-data = elastic_client.get(index="tabla2",id=3)
+    elastic_client.index(index=tabla, id=i, document={'numero':float(r)})
+data = elastic_client.get(index=tabla,id=3)
 print(str(data['_source']['numero']))
-elastic_client.indices.refresh(index="tabla2")
-forma = 1
+elastic_client.indices.refresh(index=tabla)
+forma = 0
 if forma ==0:
-    diccionario_bbdd = elastic_client.search(index="tabla2")
-    #print(diccionario_bbdd['hits']['hits']['_source']['numero'])
+    diccionario_bbdd = elastic_client.search(index=tabla)
+    print(str(diccionario_bbdd))
     def media(sum_values,num_values):
         return sum_values/num_values
         
@@ -33,7 +33,7 @@ if forma ==0:
     print(sum_values)
     print(num_values)
 else:
-    diccionario_bbdd = elastic_client.search(index="tabla2", filter_path=['hits.hits._source'])
+    diccionario_bbdd = elastic_client.search(index=tabla, filter_path=['hits.hits._source'])
     def media(sum_values,num_values):
         return sum_values/num_values
         
