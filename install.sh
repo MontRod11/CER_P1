dc26b21c2386c49d4afe84306e12c007ddbcdbf8
MontRod11 <laura.montalvo@edu.uah.es>
2021-10-25 15:29:32 +0000

    v4

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
 sudo apt update
 sudo apt install -y elasticsearch
-pip install elasticsearch
-pip install beebotte
+#pip install elasticsearch
+#pip install beebotte
