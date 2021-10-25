d1691f647330fc9ada630cdb3d718b31e7f7c3b6
MontRod11 <laura.montalvo@edu.uah.es>
2021-10-25 17:37:47 +0000

    prueba_conjuntav4

--- serv_app.py
+++ serv_app.py
@@ -6,6 +6,9 @@ from beebotte import *
 
 I_WRITE = 0
 # primera_vez = 1
+login = True
+medialocal_global = "No se puede obtener este valor sin estar registrado"
+mediainternet_global = "No se puede obtener este valor sin estar registrado"
 
 elastic_client = Elasticsearch([{'host':'localhost','port':9200}])
 token = 'token_5YNOoMGF3pj5EP1f'
@@ -18,10 +21,10 @@ tabla = "tabla2"
 elastic_client.indices.create(index=tabla, ignore=400)
 elastic_client.indices.delete(index=tabla, ignore=[400,404])
 
-@app.route("/")#,methods=['GET'])
+@app.route("/")
 def inicio():
     r = re.compile('\d*\.?\d*<br>').findall(requests.get('https://www.numeroalazar.com.ar/').text)[0][:-4]
-    return render_template('index.html',num_aleat=r)
+    return render_template('index.html',num_aleat=r, mean_local = medialocal_global, mean_beebotte=mediainternet_global)
 
 @app.route("/hello")
 def hello():
@@ -30,7 +33,7 @@ def hello():
         print(str(data))
     lectura = bbdd.read(limit=I_WRITE) # LEE TODA LA BASE DE DATOS DE INTERNET
     print(lectura)
-    return render_template('/laura/index.html',num_aleat=str(data))
+    return render_template('/laura/index.html',num_aleat=str(data), mean_local = medialocal_global, mean_beebotte=mediainternet_global)
 
 
 @app.route("/entrada")
@@ -41,14 +44,71 @@ def register():
     return 'registro'
 @app.route("/media_local") 
 def local_mean():
-    return 'media_local'
+    global medialocal_global
+    diccionario_bbdd = elastic_client.search(index=tabla)
+    print(str(diccionario_bbdd))
+    def media(sum_values,num_values):
+        return sum_values/num_values
+        
+    def get_values(dict):
+        acum = 0
+        for i in range(len(dict['hits']['hits'])):
+            value = dict['hits']['hits'][i]['_source']['numero']
+            print(str(i)+' numero de la lista: '+str(value))
+            acum = acum + value
+        return acum, len(dict['hits']['hits'])
+
+    sum_values, num_values = get_values(diccionario_bbdd)
+    mean = media(sum_values,num_values)
+
+    print('La media es:'+str(mean))
+    print(sum_values)
+    print(num_values)
+    if login == True:
+        medialocal_global = str(mean)
+        return render_template('index.html',num_aleat=re.compile('\d*\.?\d*<br>').findall(requests.get('https://www.numeroalazar.com.ar/').text)[0][:-4], 
+                                mean_local=medialocal_global,mean_beebotte=mediainternet_global)
+    else:
+        medialocal_global = "No se puede obtener este valor sin estar registrado"
+        return render_template('index.html',num_aleat=re.compile('\d*\.?\d*<br>').findall(requests.get('https://www.numeroalazar.com.ar/').text)[0][:-4], 
+                                mean_local=medialocal_global,mean_beebotte=mediainternet_global)
+
 @app.route("/media_internet") 
 def internet_mean():
-    return 'media_internet'    
+    global mediainternet_global
+    lectura = bbdd.read(limit=I_WRITE) # LEE TODA LA BASE DE DATOS DE INTERNET
+    print(str(lectura))
+
+    def media(sum_values,num_values):
+        return sum_values/num_values
+        
+    def get_values(dict):
+        acum = 0
+        for i in range(len(dict)):
+            value = dict[0]['data']
+            print(str(i)+' numero de la lista: '+str(value))
+            acum = acum + value
+        return acum, len(dict)
+
+    sum_values, num_values = get_values(lectura)
+    mean = media(sum_values,num_values)
+
+    print('La media es:'+str(mean))
+    print(sum_values)
+    print(num_values)
+    if login == True:
+        mediainternet_global = str(mean)
+        return render_template('index.html',num_aleat=re.compile('\d*\.?\d*<br>').findall(requests.get('https://www.numeroalazar.com.ar/').text)[0][:-4], mean_local=medialocal_global,
+                                mean_beebotte=mediainternet_global)
+    else:
+        mediainternet_global = "No se puede obtener este valor sin estar registrado"
+        return render_template('index.html',num_aleat=re.compile('\d*\.?\d*<br>').findall(requests.get('https://www.numeroalazar.com.ar/').text)[0][:-4], mean_local=medialocal_global,
+                                mean_beebotte=mediainternet_global)
+    # return 'media_internet'    
 @app.route("/graficas") 
 def graphs():
     return 'graficas'    
-    
+
 def get_num_aleatorio():
     global I_WRITE
     while True: 
