b3a2b25d8115f23bb6e87959fa2a98c5711bdf3f
MontRod11 <laura.montalvo@uah.es>
2021-11-07 12:35:59 +0000

    versionFINAL_app

--- serv_app.py
+++ serv_app.py
@@ -5,39 +5,35 @@ from flask.helpers import total_seconds
 from threading import Thread
 from beebotte import *
 
-
+# Variables de escritura en las bases de datos
 I_WRITE = 0
 I_WRITE_NAMES = 0
+# Variable que indica si hay sesión iniciada o no
 login_var = False
+# Variable global de usuario, media local y media global y numero de veces que se accede al resultado de la media.
 user = "Iniciar Sesión"
 medialocal_global = "No se puede obtener este valor sin estar registrado"
 mediainternet_global = "No se puede obtener este valor sin estar registrado"
-bad_pass = 0
-ya_registrado = 0
 num_veces_elastic = 0
 num_veces_beebotte =  0
+# Variables de control
+bad_pass = 0
+ya_registrado = 0
 
-# CREAR NUEVO RECURSO CADA VEZ, SE PUEDE HACER USANDO EL CÓDIGO COMENTADO SI SE PONE LA API Y SECRET KEY
 
+# Creación del cliente de la base de datos local. Esta se aloja en el puerto 9200
 elastic_client = Elasticsearch([{'host':'localhost','port':9200}])
+# Creación del cliente de la base de datos de internet, usando un token y hostname en vez de la API Key y SECRET Key.
 token = 'token_5YNOoMGF3pj5EP1f'
 hostname = 'api.beebotte.com'
 bclient = BBT(token=token,hostname=hostname)
 recurso="bbddserver_2"
-#bclient.deleteResource('cer_bbddserver', recurso)
-#resource_bbdd = Resource(bclient,'cer_bbddserver',recurso)
-# bclient.addResource( 
-#   channel = 'cer_bbddserver',
-#   name = recurso,
-#   vtype = BBT_Types.Number,
-#   label = "Base de datos internet prueba",
-#   description = "Base de datos de internet prueba",
-#   sendOnSubscribe = False
-# )
 
+# Importación de la clase "Flask" y creación de la instancia, necesario para que Flask sepa donde buscar los recursos como "templates" y ficheros estáticos.
 app = Flask(__name__)
 app.secret_key = 'lausi'
 
+# Creación de las tablas de la base de datos local en las que se van a guardar los números aleatorios, datos de usuario, y número de veces que se almacena la media.
 tabla = "tabla2"
 elastic_client.indices.create(index=tabla, ignore=400)
 elastic_client.indices.delete(index=tabla, ignore=[400,404])
@@ -46,7 +42,7 @@ tabla_num_medias = "tabla_medias"
 elastic_client.indices.create(index=tabla_nombres, ignore=400)
 elastic_client.indices.create(index=tabla_num_medias, ignore=400)
 
-@app.route("/")#,methods = ["POST"])
+@app.route("/") # El uso de route() dice a Flask en que URL se debe ejecutar la función.
 def inicio():
     """
     Esta función es la funcion base e inicial del programa, en este caso solo se actualiza el indice sacando un número aleatorio de la pagina web numero al azar
@@ -57,7 +53,11 @@ def inicio():
 @app.route("/umbral1",methods = ["POST"])
 def umbral1():
     """
-    Esta función es la funcion que permite mostral el umbral histórico que supera cierto umbral
+    Esta función es la funcion que permite mostral el umbral histórico que supera cierto umbral:
+    - Si no se ha introducido un número entonces devuelve una página que indica error.
+    - Si la base de datos está vacía entonces indica un '0'.
+    - Si solo hay 5 números en la base de datos solo muestra aquellos que superan el umbral.
+    - Si hay más de 5 números en la base de datos, muestra los últimos 5 que superan el umbrales.
     """
     umbral=request.form['umbral']
     if umbral == '':
@@ -89,8 +89,8 @@ def umbral1():
 @app.route("/hello")
 def hello():
     """ 
-    Esta funcion es una función de prueba que permite ver en la terminal las bases de datos y sus contenidos y muestra en el índice el último elemento que se
-    ha incluido en la base de datos local.
+    Esta funcion es una función de prueba que permite ver en la terminal las bases de datos y sus contenidos y muestra en la página el último elemento que se
+    ha incluido en la base de datos local, y de internet.
     """
     print("\nBase de datos local")
     for i in range(I_WRITE):
@@ -113,6 +113,16 @@ def login():
 
 @app.route("/loggeado",methods = ["POST"])
 def loggeado():
+    """
+    En esta función se realiza el login como tal:
+    - Si no se ha introducido usuario entonces se devuelve una página web en la que se indica que se debe introducir un nombre de usuario correcto.
+    - Si el usuario ya esta con la sesión iniciada se devuelve una página en la que se indica que el usuario ya está loggeado.
+    - Si la base de datos de usuario está vacía se indica que el usuario debe registrarse previamente.
+    - Si hay usuarios registrados en la base de datos entonces se comprueba si el usuario está registrado:
+            · Si el usuario está registrado se comprueba la contraseña (si la contraseña cifrada coincide con la almacenada en la base de datos entonces
+            se inicia sesión, si la contraseña no coincide entonces se indica al usuario que la contraseña introducida es incorrecta)
+            · Si el usuario no está registrado se le indica que se registre previamente. 
+    """
     global I_WRITE_NAMES
     global login_var
     global user
@@ -172,7 +182,16 @@ def signin():
 
 @app.route("/registrado",methods = ["POST"])
 def registrado():
-    """Comprobar que existe el usuario en la base de datos y comprobar la constaseña devolver en user el session['email'] ya que es un str """
+    """
+    En esta función se realiza el registro como tal:
+    - Si no se ha introducido usuario entonces se devuelve una página web en la que se indica que se debe introducir un nombre de usuario correcto.
+    - Si la base de datos de usuario está vacía se comienza el proceso de cifrado de la contraseña y se almancena usuario, sal y contraseña cifrada en la base de datos de 
+    nombres y se crea una base de datos donde se asocia a cada usuario una variable en la que se almacena el número de veces que pide la media (inicializado a cero).
+    - Si hay usuarios registrados en la base de datos entonces se comprueba si el usuario está registrado:
+            · Si el usuario está registrado entonces devuelve una página en la que indica al usuario que ya esta registrado y que puede iniciar sesión.
+            · Si el usuario no está registrado entonces se comienza el proceso de cifrado de la contraseña y se almancena usuario, sal y contraseña cifrada en la base de datos de 
+            nombres y se crea una base de datos donde se asocia a cada usuario una variable en la que se almacena el número de veces que pide la media (inicializado a cero).
+    """
     global I_WRITE_NAMES
     global ya_registrado
     # global user
@@ -215,6 +234,11 @@ def registrado():
     
 @app.route("/logout") 
 def logout():
+    """
+    Funcion en la que se realiza el logout, si no hay ningún usuario loggeado entonces devuelve un indice en el que se indica que debe iniciar sesión, y si hay usuario loggeado
+    entonces guarda en la base de datos que almacena el número de veces que cada usuario pide la base de datos.
+    Reinicia el resto de variables, cierra la sesuón e indica que no hay usuario loggeado.
+    """
     global login_var
     global medialocal_global
     global mediainternet_global
@@ -245,7 +269,7 @@ def logout():
 @app.route("/media_local") 
 def local_mean():
     """
-    Esta función realiza el cálculo de la media de la base de datos local y actualiza la página web sacando su valor, si el usuario está loggeado y, si no lo 
+    Esta función realiza el cálculo de la media de la base de datos local y actualiza la página web sacando su valor, si el usuario está loggeado, y, si no lo 
     está, entonces no saca nada.
     """
     global medialocal_global
@@ -254,6 +278,7 @@ def local_mean():
         data = []
         print("\nCalculo de la media en la base de datos local:")
         for i in range(I_WRITE):
+            # saca todos los números que hay en la base de datos
             data.append(elastic_client.get(index=tabla,id=i)['_source']['numero'])
             print('Elemento '+str(i)+' : '+str(data))
 
@@ -268,12 +293,14 @@ def local_mean():
                 acum = acum + value
             return acum, len(dict)
 
+        # calculo de la media
         sum_values, num_values = get_values(data)
         mean = media(sum_values,num_values)
 
         print('La media es:'+str(mean))
         print('Acumulacion: '+str(sum_values))
         print('Nº de valores: '+str(num_values)+"\n")
+        # aumenta el número de veces que se ha pedido la media local
         num_veces_elastic =  str(int(num_veces_elastic) +1)
         medialocal_global = str(mean)
         return render_template('index.html',num_aleat=re.compile('\d*\.?\d*<br>').findall(requests.get('https://www.numeroalazar.com.ar/').text)[0][:-4], 
@@ -286,12 +313,13 @@ def local_mean():
 @app.route("/media_internet") 
 def internet_mean():
     """
-    Esta función realiza el cálculo de la media de la base de datos de internet y actualiza la página web sacando su valor, si el usuario está loggeado y, 
+    Esta función realiza el cálculo de la media de la base de datos de internet y actualiza la página web sacando su valor, si el usuario está loggeado, y, 
     si no lo está, entonces no saca nada.
     """
     global num_veces_beebotte
     global mediainternet_global
     if login_var == True:
+        # saca todos los números que hay en la base de datos
         lectura = bclient.read('cer_bbddserver',recurso,limit=I_WRITE)
         for i in range(len(lectura)):
             value = lectura[i]['data']
@@ -310,9 +338,11 @@ def internet_mean():
                 acum = acum + value
             return acum, len(dict)
 
+        # calculo de la media
         sum_values, num_values = get_values(lectura)
         mean = media(sum_values,num_values)
 
+         # aumenta el número de veces que se ha pedido la media de internet
         print('La media es:'+str(mean))
         print('Acumulacion: '+str(sum_values))
         print('Nº de valores: '+str(num_values)+"\n")
@@ -327,6 +357,7 @@ def internet_mean():
 
 @app.route("/graficas") 
 def graphs():
+    """ Si hay usuario loggeado devuelve la gráfica asociada al recurso creado para la base de datos de internet, si no lo está entonces devuelve el propio índice """
     if login_var == True:
         return redirect("https://beebotte.com/dash/a4986a00-38b9-11ec-954b-39d34f82886a?shareid=shareid_s410ZmUDNJAKMbfq") 
     else: 
@@ -334,6 +365,7 @@ def graphs():
                                 mean_beebotte=mediainternet_global,user=user,num_veces_elastic=num_veces_elastic,num_veces_beebotte=num_veces_beebotte)
 
 def get_num_aleatorio():
+    """ función que se ejecuta cada dos minutos gracias a un hilo donde se va almacenando un número aleatorio en las distintas bases de datos"""
     global I_WRITE
     while True: 
         r = re.compile('\d*\.?\d*<br>').findall(requests.get('https://www.numeroalazar.com.ar/').text)[0][:-4]
@@ -343,8 +375,7 @@ def get_num_aleatorio():
         time.sleep(120) #120
 
 if __name__ == "__main__":
-
+    """ Función main, inicializa el hilo que va a introducir números en la base de datos cada dos minutos y inicializa la aplicación del servidor web en el puerto 5000 del localhost """
     hilo1 = Thread(target=get_num_aleatorio, daemon=True)
     hilo1.start()
     app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
-    #app.run(host='0.0.0.0', port=5000, debug=True)
