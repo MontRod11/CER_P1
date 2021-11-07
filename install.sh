149524fd1624b5a292a277aaf9c304813d2f7a29
MontRod11 <laura.montalvo@uah.es>
2021-11-07 12:36:38 +0000

    versionFINAL_install

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 sudo apt update && apt upgrade -y
-sudo apt install python3-pip
+sudo apt install -y python3 python3-pip
 pip3 install -r requirements.txt
-echo 'export PATH="/home/$USER/.local/bin:$PATH""' >> .bashrc
+echo 'export PATH="/home/$USER/.local/bin:$PATH"' >> ../.bashrc
 source .bashrc
 curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
